885805575a8442d03ffcbcde4675396f07d78c58
Thierry Castro <thierrycastro555@gmail.com>
2022-11-19 13:38:36 -0300

    rota de login criada

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const route = express();
 
+const login = require("./controllers/login");
+const authorization = require("./middleware/authorization");
 const { registerUser, detailUser } = require("./controllers/users");
 
 route.post("/users", registerUser);
+
+route.post("/login", login);
+
+route.use(authorization);
+
 route.get("/user", detailUser);
 
 module.exports = route;
