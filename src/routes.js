9711d3408d4cf8a590ea3d1df4b755e9dc737329
Thierry Castro <thierrycastro555@gmail.com>
2022-11-19 12:12:51 -0300

    registerUser criado

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const route = express();
 
+const { registerUser } = require("./controllers/users");
+
+route.post("/users", registerUser);
+
 module.exports = route;
