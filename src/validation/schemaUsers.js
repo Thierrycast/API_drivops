0363ae248d2e76661f1e1a23fb7703e057f09c03
Thierry Castro <thierrycastro555@gmail.com>
2022-11-19 12:39:23 -0300

    configurando yup validate

--- src/validation/schemaUsers.js
+++ src/validation/schemaUsers.js
@@ -5,3 +5,7 @@ const schemaRegisterUser = yup.object().shape({
   email: yup.string().required().email(),
   senha: yup.string().required().min(6).max(10).trim(),
 });
+
+module.exports = {
+  schemaRegisterUser,
+};
